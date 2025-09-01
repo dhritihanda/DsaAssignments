a18065eceab7b54e8bcb72a0c5859624170d5f31
Dhriti <dhanda_be24@thapar.edu>
2025-09-02 01:34:52 +0530

    Create q2.cpp

---
+++ assignment 3/q2.cpp
@@ -0,0 +1,20 @@
+q2
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#define SIZE 100
+
+int main() {
+    char str[SIZE];
+    char stack[SIZE];
+    int top = -1;
+    cout << "Enter a string: ";
+    cin >> str;
+    int n = strlen(str);
+    for (int i = 0; i < n; i++) stack[++top] = str[i];
+    cout << "Reversed: ";
+    while (top != -1) cout << stack[top--];
+    cout << "\n";
+    return 0;
+}
